65e0cf27fa2727b386e68c2bb50e15aeea8a4d22
jankuca <jan@jankuca.com>
2025-03-02 21:59:05 +0100

    feat: auto-start exporting on init

--- autoimportexport.lrdevplugin/LrInitPlugin.lua
+++ autoimportexport.lrdevplugin/LrInitPlugin.lua
@@ -1,6 +1,9 @@
+local LrApplication = import 'LrApplication'
 local LrDialogs = import 'LrDialogs'
+local LrTasks = import 'LrTasks'
 
 local ExportSettings = require 'ExportSettings'
+local Exporter = require 'Exporter'
 
 local exportSettings = ExportSettings.loadExportSettings()
 if exportSettings then
@@ -9,7 +12,8 @@ else
     LrDialogs.showBezel("Auto import/export plugin loaded without export settings", 2)
 end
 
--- local LrCatalog = LrApplication.activeCatalog()
--- local processAll = true
-
--- processLightroomFolders(LrCatalog, processAll, exportSettings)
+LrTasks.startAsyncTask(function()
+    local LrCatalog = LrApplication.activeCatalog()
+    local processAll = true
+    Exporter.processLightroomFolders(LrCatalog, processAll, exportSettings)
+end)
