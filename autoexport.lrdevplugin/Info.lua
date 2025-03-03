1d8bb61c76c9051a13c88fdbfecf053dec02b2e3
jankuca <jan@jankuca.com>
2025-03-03 22:28:26 +0100

    feat: rebrand as "Auto-export" (com.jankuca.lightroom.autoexport)

---
+++ autoexport.lrdevplugin/Info.lua
@@ -0,0 +1,22 @@
+return {
+    VERSION = {
+        major = 1,
+        minor = 0,
+        revision = 0,
+        build = 0
+    },
+
+    LrToolkitIdentifier = "com.jankuca.lightroom.autoexport",
+    LrPluginName = "Auto-export",
+
+    LrSdkVersion = 3.0,
+    LrSdkMinimumVersion = 1.3, -- minimum SDK version required by this plug-in
+
+    LrExportMenuItems = {
+        title = "Auto-export settings",
+        file = "ExportMenuItem.lua"
+    },
+
+    LrInitPlugin = "LrInitPlugin.lua",
+    LrForceInitPlugin = true
+}
